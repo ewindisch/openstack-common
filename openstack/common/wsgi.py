openstack-archive/oslo-incubator

Make wsgi.Server comply with service.Launcher interface
fixes LP 1087369

Change-Id: Icf222b5bffe1f40a5c325ffe2a1b397f22713c00

openstack/common/wsgi.py
@@ -55,19 +55,25 @@ class Service(service.Service):
     Launcher classes in service.py.
     """
 
-    def __init__(self, threads=1000):
+    def __init__(self, application, port,
+                 host='0.0.0.0', backlog=128, threads=1000):
+        self.application = application
+        self.initial_port = port
+        self.initial_host = host
+        self.backlog = backlog
         super(Service, self).__init__(threads)
 
-    def start(self, application, port, host='0.0.0.0', backlog=128):
+    def start(self):
         """Start serving this service using the provided server instance.
 
         :returns: None
 
         """
         super(Service, self).start()
-        socket = eventlet.listen((host, port), backlog=backlog)
+        socket = eventlet.listen((self.initial_host, self.initial_port),
+                                 backlog=self.backlog)
         (self._host, self._port) = socket.getsockname()
-        self.tg.add_thread(self._run, application, socket)
+        self.tg.add_thread(self._run, self.application, socket)
 
     @property
     def host(self):

ewindisch/openstack-common

openstack/common/wsgi.py
@@ -55,27 +55,32 @@ class Service(service.Service):
     Launcher classes in service.py.
     """
 
-    def __init__(self, threads=1000):
+    def __init__(self, application, port,
+                 host='0.0.0.0', backlog=128, threads=1000):
+        self.application = application
+        self._port = port
+        self._host = host
+        self.backlog = backlog
         super(Service, self).__init__(threads)
 
-    def start(self, application, port, host='0.0.0.0', backlog=128):
+    def start(self):
         """Start serving this service using the provided server instance.
 
         :returns: None
 
         """
         super(Service, self).start()
-        socket = eventlet.listen((host, port), backlog=backlog)
-        (self._host, self._port) = socket.getsockname()
-        self.tg.add_thread(self._run, application, socket)
+        self._socket = eventlet.listen((self._host, self._port),
+                                       backlog=self.backlog)
+        self.tg.add_thread(self._run, self.application, self._socket)
 
     @property
     def host(self):
-        return self._host
+        return self._socket.getsockname()[0] if self._socket else self._host
 
     @property
     def port(self):
-        return self._port
+        return self._socket.getsockname()[1] if self._socket else self._port
 
     def stop(self):
         """Stop serving this API.